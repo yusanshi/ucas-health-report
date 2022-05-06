5a6237e3b0a7e2ba7d03b4c465b6690a4956f9b0
yusanshi <meet.leiyu@gmail.com>
2022-05-06 16:48:53 +0800

    Fix waiting time

--- get_health_code.py
+++ get_health_code.py
@@ -12,10 +12,10 @@ args = parser.parse_args()
 d = u2.connect()
 
 d.app_start('com.iflytek.oshall.ahzwfw')
-sleep(2)
+sleep(5)
 assert d(text='安康码').count == 1
 d(text='安康码').click()
-sleep(2)
+sleep(5)
 if d(text='打开系统定位服务').count > 0:
     assert d(text='取消').count == 1
     d(text='取消').click()
@@ -23,35 +23,49 @@ if d(text='打开系统定位服务').count > 0:
 assert d(text='切换国康码').count == 1
 d.screenshot(args.akm_path)
 
-d.open_url("https://xc.caict.ac.cn/")
+d.open_url("https://xc.caict.ac.cn/#/result")
 sleep(5)
-edit_areas = d.xpath("//android.widget.EditText").all()
-assert len(edit_areas) == 3
-phone_area = edit_areas[0]
-captcha_area = edit_areas[1]
-checkbox_areas = d.xpath("//android.widget.CheckBox").all()
-assert len(checkbox_areas) == 1
-checkbox_area = checkbox_areas[0]
-button_areas = d.xpath("//android.widget.Button").all()
-assert len(button_areas) == 2
-get_captcha_area = button_areas[0]
-confirm_area = button_areas[1]
 
-phone_area.click()
-sleep(1)
-d.send_keys(PHONE_NUMBER)
-sleep(1)
-get_captcha_area.click()
-sleep(1)
-captcha = input('Please input the captcha: ')
-captcha_area.click()
-sleep(1)
-d.send_keys(captcha)
-sleep(1)
-checkbox_area.click()
-sleep(1)
-confirm_area.click()
-sleep(2)
+for i in range(5):
+    if len(d.xpath('%的动态行程卡').all()) == 1:
+        need_login = False
+        break
+    elif len(d.xpath("//android.widget.EditText").all()) == 3:
+        need_login = True
+        break
+    print('Retrying')
+    sleep(3)
+else:
+    raise
+
+if need_login:
+    edit_areas = d.xpath("//android.widget.EditText").all()
+    assert len(edit_areas) == 3
+    phone_area = edit_areas[0]
+    captcha_area = edit_areas[1]
+    checkbox_areas = d.xpath("//android.widget.CheckBox").all()
+    assert len(checkbox_areas) == 1
+    checkbox_area = checkbox_areas[0]
+    button_areas = d.xpath("//android.widget.Button").all()
+    assert len(button_areas) == 2
+    get_captcha_area = button_areas[0]
+    confirm_area = button_areas[1]
+
+    phone_area.click()
+    sleep(1)
+    d.send_keys(PHONE_NUMBER)
+    sleep(1)
+    get_captcha_area.click()
+    sleep(1)
+    captcha = input('Please input the captcha: ')
+    captcha_area.click()
+    sleep(1)
+    d.send_keys(captcha)
+    sleep(1)
+    checkbox_area.click()
+    sleep(1)
+    confirm_area.click()
+    sleep(3)
 
 assert len(d.xpath('%的动态行程卡').all()) == 1
 d.screenshot(args.xck_path)
--- upload_and_apply.py
+++ upload_and_apply.py
@@ -14,7 +14,6 @@ from config import CAS_USERNAME, CAS_PASSWORD
 LOGIN_URL = 'https://passport.ustc.edu.cn/login?service=https%3A%2F%2Fweixine.ustc.edu.cn%2F2020%2Fcaslogin'
 UPLOAD_URL = 'https://weixine.ustc.edu.cn/2020/upload/xcm'
 APPLY_URL = 'https://weixine.ustc.edu.cn/2020/apply/daliy'
-RETRY = 5
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--akm_path', type=str, default='akm.png')
@@ -23,7 +22,6 @@ args = parser.parse_args()
 
 
 def main():
-
     options = Options()
     options.add_argument('--no-sandbox')
     # options.add_argument('--headless')
@@ -35,83 +33,78 @@ def main():
     )
 
     service = Service('/usr/lib/chromium-browser/chromedriver')
-    with webdriver.Chrome(service=service, options=options) as driver:
-        driver.get(LOGIN_URL)
+    driver = webdriver.Chrome(service=service, options=options)
 
-        sleep(5)
-        WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located((By.ID, 'username')))
+    driver.get(LOGIN_URL)
 
-        driver.find_element(By.ID, 'username').send_keys(CAS_USERNAME)
-        driver.find_element(By.ID, 'password').send_keys(CAS_PASSWORD)
-        if driver.find_elements(By.ID, 'validate'):
-            # If has the validating code
-            image = driver.find_element(By.CSS_SELECTOR,
-                                        '.validate img').screenshot_as_png
-            reader = easyocr.Reader(['en'])
-            captcha = reader.readtext(image, detail=0,
-                                      allowlist='0123456789')[0]
-            print('Captcha recognized: {}'.format(captcha))
-            driver.find_element(By.ID, 'validate').send_keys(captcha)
+    sleep(2)
+    WebDriverWait(driver,
+                  20).until(EC.presence_of_element_located(
+                      (By.ID, 'username')))
 
-        driver.find_element(By.ID, 'login').click()
+    driver.find_element(By.ID, 'username').send_keys(CAS_USERNAME)
+    driver.find_element(By.ID, 'password').send_keys(CAS_PASSWORD)
+    if driver.find_elements(By.ID, 'validate'):
+        # If has the validating code
+        image = driver.find_element(By.CSS_SELECTOR,
+                                    '.validate img').screenshot_as_png
+        reader = easyocr.Reader(['en'])
+        captcha = reader.readtext(image, detail=0, allowlist='0123456789')[0]
+        print('Captcha recognized: {}'.format(captcha))
+        driver.find_element(By.ID, 'validate').send_keys(captcha)
 
-        sleep(5)
-        driver.get(UPLOAD_URL)
-        sleep(5)
+    driver.find_element(By.ID, 'login').click()
 
-        def get_form(text):
-            forms = [
-                x for x in driver.find_elements(By.CLASS_NAME, 'form-group')
-                if text in x.text
-            ]
-            assert len(forms) == 1
-            return forms[0]
+    sleep(2)
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, "//a[text()='退出']")))
 
-        xck_form = get_form('选择14天大数据行程卡')
-        xck_form.find_element(By.TAG_NAME, 'input').send_keys(args.xck_path)
-        sleep(2)
-        assert '删除' in xck_form.text
-        akm_form = get_form('上传安康码')
-        akm_form.find_element(By.TAG_NAME, 'input').send_keys(args.akm_path)
-        sleep(2)
-        assert '删除' in akm_form.text
+    driver.get(UPLOAD_URL)
+    sleep(2)
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//h5[text()='选择14天大数据行程卡：']")))
 
-        sleep(5)
-        driver.get(APPLY_URL)
-        sleep(5)
-        driver.find_element(By.XPATH, "//span[text()='前往先研院']").click()
-        sleep(1)
-        driver.find_element(
-            By.XPATH,
-            "//span[text()='我已知悉以上规定并保证按要求执行，做好个人防护，少出行不聚集。']").click()
-        sleep(1)
-        driver.find_element(By.XPATH, "//button[text()='进出校报备']").click()
-        sleep(1)
-        driver.find_element(By.XPATH, "//span[text()='先研院']").click()
-        sleep(1)
-        driver.find_element(By.XPATH,
-                            "//input[@name='reason']").send_keys('科研、锻炼身体')
-        sleep(1)
-        driver.find_element(By.XPATH, "//button[text()='提交报备']").click()
-        sleep(1)
-        assert len(driver.find_elements(By.XPATH,
-                                        "//strong[text()='在校可跨校区']")) == 1
+    xck_form = driver.find_element(By.XPATH,
+                                   "//h5[text()='选择14天大数据行程卡：']/parent::div")
+    xck_form.find_element(By.TAG_NAME, 'input').send_keys(args.xck_path)
+    sleep(2)
+    assert '删除' in xck_form.text
 
+    akm_form = driver.find_element(By.XPATH,
+                                   "//h5[text()='上传安康码：']/parent::div")
+    akm_form.find_element(By.TAG_NAME, 'input').send_keys(args.akm_path)
+    sleep(2)
+    assert '删除' in akm_form.text
 
-def notify_myself(message):
-    # TODO email, Telegram bot, etc.
-    print(message)
+    driver.get(APPLY_URL)
+    sleep(2)
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, "//h5[text()='进出校原因：']")))
+
+    driver.find_element(By.XPATH, "//span[text()='前往先研院']").click()
+    sleep(1)
+    driver.find_element(
+        By.XPATH, "//span[text()='我已知悉以上规定并保证按要求执行，做好个人防护，少出行不聚集。']").click()
+    sleep(1)
+    driver.find_element(By.XPATH, "//button[text()='进出校报备']").click()
+
+    sleep(2)
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.XPATH, "//button[text()='提交报备']")))
+
+    driver.find_element(By.XPATH, "//span[text()='先研院']").click()
+    sleep(1)
+    driver.find_element(By.XPATH,
+                        "//input[@name='reason']").send_keys('科研、锻炼身体')
+    sleep(1)
+    driver.find_element(By.XPATH, "//button[text()='提交报备']").click()
+
+    sleep(2)
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//strong[text()='在校可跨校区']")))
 
 
 if __name__ == '__main__':
-    for i in range(RETRY):
-        try:
-            main()
-            print('Success')
-            break
-        except Exception as e:
-            print('Failed for {} times: {}'.format(i + 1, e))
-            sleep(10 * (i + 1))
-    else:
-        notify_myself('Failed')
+    main()
