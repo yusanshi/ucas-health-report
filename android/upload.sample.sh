0658edf1825738d77f16b1c6f2996ed63da0b9ed
yusanshi <meet.leiyu@gmail.com>
2022-05-24 00:04:25 +0800

    Fix bug with shell return code

--- android/upload.sample.sh
+++ android/upload.sample.sh
@@ -1,4 +1,5 @@
 set -u
+set -e
 
 # This script will be run after getting the health codes
 # `$AKM_PATH`, `$XCK_PATH` and `$HSM_PATH`(if not empty) will be expanded to the full path
@@ -6,3 +7,5 @@ scp $AKM_PATH user@host:health-code/
 scp $XCK_PATH user@host:health-code/
 # only run if `$HSM_PATH` is not empty
 [[ !  -z  $HSM_PATH  ]] && scp $HSM_PATH user@host:health-code/
+
+exit 0
